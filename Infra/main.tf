e892b4926528c53206fe53783058ce7b7b4663a2
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-14 00:37:14 +0530

    erty

--- Infra/main.tf
+++ Infra/main.tf
@@ -63,7 +63,39 @@ module "ecs_nodejs" {
 }
 
 resource "aws_ecs_task_definition" "nodejs" {
-  # resource details...
+  family                   = "nodejs-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["EC2"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = var.execution_role_arn
+
+  container_definitions = jsonencode([{
+    name      = "nodejs-app"
+    image     = "your-nodejs-image"  # Replace this with the correct image
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+    mountPoints = [{
+      sourceVolume  = "nodejs-volume"
+      containerPath = "/mnt/data"
+    }]
+  }])
+
+  volume {
+    name = "nodejs-volume"
+    efs_volume_configuration {
+      file_system_id          = var.file_system_id
+      transit_encryption      = "ENABLED"
+      authorization_config {
+        access_point_id = split("/", var.efs_access_point_arn)[length(split("/", var.efs_access_point_arn)) - 1]
+        iam             = "ENABLED"
+      }
+    }
+  }
 }
+
 
 
