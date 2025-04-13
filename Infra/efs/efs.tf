5dfa7471999801d21a9647a3b4c2adcf457d58ef
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-14 00:15:58 +0530

    efs

--- Infra/efs/efs.tf
+++ Infra/efs/efs.tf
@@ -12,14 +12,9 @@ resource "aws_efs_mount_target" "this" {
   file_system_id = aws_efs_file_system.this.id
   subnet_id      = var.subnet_ids[count.index]
   security_groups = [var.ecs_sg_id]  # Pass ecs_sg_id as a variable
-
-  tags = {
-    Name = "efs-mount-target-${count.index}"
-  }
 }
 
 resource "aws_security_group_rule" "ecs_service" {
-  # Example to create ECS security group rules if needed
   type        = "ingress"
   from_port   = 0
   to_port     = 0
@@ -52,6 +47,17 @@ resource "aws_security_group" "ecs_service" {
     Name = "ecs-service-sg"
   }
 }
+
+# Add tags for mount targets separately
+resource "aws_tags" "efs_mount_target_tags" {
+  count = length(var.subnet_ids)
+  
+  resource_id = aws_efs_mount_target.this[count.index].id
+  tags = {
+    Name = "efs-mount-target-${count.index}"
+  }
+}
+
 
 
 
