539d926ad00fee4704acfb8b14368271049bafcf
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-13 22:51:48 +0530

    fghjkl

--- Infra/efs/efs.tf
+++ Infra/efs/efs.tf
@@ -1,24 +1,24 @@
-resource "aws_efs_file_system" "mongo_efs" {
-  creation_token = "mongo-efs"
-  lifecycle_policy {
-    transition_to_ia = "AFTER_7_DAYS"
+resource "aws_efs_file_system" "example" {
+  creation_token = "example-token"
+  performance_mode = "generalPurpose"
+  encrypted = true
+}
+
+resource "aws_efs_access_point" "example" {
+  file_system_id = aws_efs_file_system.example.id
+  root_directory {
+    creation_info {
+      owner_gid = 1001
+      owner_uid = 1001
+      permissions = "750"
+    }
+    path = "/export/mydata"
   }
-
-  tags = {
-    Name = "mongo-efs"
-  }
 }
 
-resource "aws_efs_mount_target" "mongo_efs_target" {
-  count           = length(var.public_subnet_ids)
-  file_system_id  = aws_efs_file_system.mongo_efs.id
-  subnet_id       = var.public_subnet_ids[count.index]
-  security_groups = [aws_security_group.efs_sg.id]
+# Output the ARN of the EFS Access Point
+output "access_point_arn" {
+  value = aws_efs_access_point.example.arn
 }
-
-output "efs_id" {
-  value = aws_efs_file_system.mongo_efs.id
-}
-
 
 
